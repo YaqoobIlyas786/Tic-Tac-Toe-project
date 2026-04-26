705e94eeab8e227caacb7d119a8edcba20634a17
YaqoobIlyas <yaqoobilyas123@gmail.com>
2026-04-26 13:49:37 +0200

    feat: Implement core tic-tac-toe gameplay
    Adds game board, turn tracking, and active player state management. Includes UI components for the board and move log.

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,47 @@
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
+
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = [...initialGameBoard.map((array) => [...array])]
+
+  for (const turn of turns) {
+    const { square, player } = turn
+    const { row, col } = square
+    gameBoard[row][col] = player
+  }
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                  >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  )
+}
+/*
+// const [gameBoard, setGameBoard] = useState(initialGameBoard)
+() => {
+                    setGameBoard((prevGameBoard) => {
+                      const updatedBoard = [
+                        ...prevGameBoard.map((innerArray) => [...innerArray]),
+                      ]
+                      updatedBoard[rowIndex][colIndex] = activePlayerSymbol
+                      return updatedBoard
+                    })
+                    onSelectSquare()
+                  }
+*/
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,16 +1,25 @@
-import { useState } from "react";
-export default function Player({initialName,symbol}){
-    const [name,setName]=useState(initialName)
-const [isEditing,setIsEditing]=useState(false);
-    return (
- <li>
-    <span className="player">
-    {isEditing?
-    <input type="text" required value={name} onChange={event=>setName(event.target.value)}/>
-    :<span className="player-name">{name}</span>}
-    <span className="player-symbol">{symbol}</span>
-  </span>
-  <button onClick={()=>setIsEditing(editing=> !editing)}>{isEditing?"Save":"Edit"}</button>
-  </li>
-    );
+import { useState } from 'react'
+export default function Player({ initialName, symbol, isActive }) {
+  const [name, setName] = useState(initialName)
+  const [isEditing, setIsEditing] = useState(false)
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            required
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        ) : (
+          <span className="player-name">{name}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={() => setIsEditing((editing) => !editing)}>
+        {isEditing ? 'Save' : 'Edit'}
+      </button>
+    </li>
+  )
 }
